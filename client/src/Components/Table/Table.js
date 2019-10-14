c2744a7c29e362249cca90aefb8d1a5f72823c3f
lesley <lesleybanadzem2017@gmail.com>
2019-10-13 20:59:38 -0700

    Added functionality to display 5 players at a time and user is able to view more or less

--- client/src/Components/Table/Table.js
+++ client/src/Components/Table/Table.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components';
 import TableHeader from './TableHeader';
 import TableBody from './TableBody';
 
-const Table = (props ) => {
+const Table = ( {playerData} ) => {
+    const [display, setDisplay] = useState([])
+    const [number, setNumber] = useState(5)
+    useEffect(() => {
+        if(number <= 5){
+            setNumber(5)
+        }
+        setDisplay(playerData.slice(0, number))
+    },[playerData, number]);
 
+    const handleNumber = (e, moreLess) => {
+        e.preventDefault();
+        if(number < 5){
+            setNumber(5)
+        }
+        else if(moreLess === 'more'){
+            setNumber(number + 5);
+        } else if(moreLess === 'less'){
+            setNumber(number - 5);
+        }
+    }
     return (
         <>
             <TableStyles  >  
                 <TableHeader />
                 <TableBody
-                    playerData = { props.playerData}
+                    playerData = { display }
                     />
             </TableStyles>
+            <button onClick={e=> handleNumber(e, 'more')}>More</button>
+            <button  onClick={e=> handleNumber(e, 'less')}>Less</button>
         </>
     )
     
